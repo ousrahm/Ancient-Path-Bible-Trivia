08217419ff892e28ab52a375bdddfa92de9a4b20
Oussama <72579926+ousrahm@users.noreply.github.com>
2021-02-23 13:55:53 -0500

    Added files via upload

---
+++ PromisedLandGameRepository/game.js
@@ -0,0 +1,10 @@
+
+var config = {
+    width: 800,
+    height: 600,
+    backgroundColor: 0x0000000
+}
+window.onload = function() {
+    // Created a new Game instance that we can configure
+    var game = new Phaser.Game(config);
+}
