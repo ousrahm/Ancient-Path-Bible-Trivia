f82f9faa6032dd6af5bab5b8caca135074fd5fd1
ousrahm <ousrahm1@gmail.com>
2021-05-05 22:07:55 -0400

    fixed bug where timing out on trivia scene prevented a listener from being turned off

--- scenes/triviaScene.js
+++ scenes/triviaScene.js
@@ -175,14 +175,13 @@ class TriviaScene extends Phaser.Scene {
         database.ref("promised-land-journey-game").child(gameState.getGameCode()).child("selectedAnswer").off("value", this.selectedListener)
         var answerRef = await database.ref("promised-land-journey-game").child(gameState.getGameCode()).child("selectedAnswer").get();
         var answer = answerRef.val();
+        console.log("Answer: " + answer);
         
         if (answer === await questions.getCorrect()) {
             this.openScene("correct");
         } else {
             this.openScene("incorrect");
         }
-        
-
     }
 
     
@@ -278,7 +277,8 @@ class TriviaScene extends Phaser.Scene {
         /** If the timer reaches 0, go to incorrect scene. */
         if (this.timedEvent.repeatCount == 0 && !this.timesUp) {
             this.timesUp = true;
-            this.openScene("incorrect");
+            this.answerWasSelected();
+            // this.openScene("incorrect");
         }
 
     }
